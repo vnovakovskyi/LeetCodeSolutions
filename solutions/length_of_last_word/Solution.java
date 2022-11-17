22c5b457dc784346c47bc7ef67b4da4da79b289c
Vadym Novakovskyi <vadym.novakovskyi@gmail.com>
2022-11-17 19:51:15 +0100

    58. Length of Last Word

---
+++ solutions/length_of_last_word/Solution.java
@@ -0,0 +1,21 @@
+package length_of_last_word;
+
+//58. Length of Last Word leetcode.com problem
+//https://leetcode.com/problems/length-of-last-word/
+
+
+class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("luffy is still joyboy"));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        String text = s.trim();
+        if (s.isEmpty()) return 0;
+
+        String [] arr = text.split(" ");
+
+        return arr[arr.length-1].length();
+    }
+}
