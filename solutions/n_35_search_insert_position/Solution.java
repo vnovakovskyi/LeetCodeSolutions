d42e06015e3d4433b6bfc482a47603f99c5393c9
Vadym Novakovskyi <vadym.novakovskyi@resurs.se>
2025-01-18 15:23:35 +0100

    35. Search Insert Position

---
+++ solutions/n_35_search_insert_position/Solution.java
@@ -0,0 +1,24 @@
+package n_35_search_insert_position;
+
+//35. Search Insert Position
+//https://leetcode.com/problems/search-insert-position/description/
+
+
+class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 7));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
